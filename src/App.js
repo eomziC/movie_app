da260a5a02adcd074fa01a4d4ca63c082df87f10
Troy <yoon6@gmail.com>
2023-03-15 11:28:16 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-    {
-        id : 1,
-        name: "Kimchi",
-        image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-        rating: 5
-    },
-    {
-        id : 2,
-        name: "bulgogi",
-        image: "https://recipe1.ezmember.co.kr/cache/recipe/2019/03/03/11baafbe81803965b17c3ab42a5992cb1.jpg",
-        rating: 4.9
-    },
-    {
-        id : 3,
-        name: "kimbap",
-        image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/02/21/f34c2f0fcd67513941d683d90050f3c01.jpg",
-        rating: 4.8
-    },
-    {
-        id : 4,
-        name: "samgyetang",
-        image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-        rating: 4
+class App extends React.Component{
+    state = {
+        count: 0
+    };
+    add = () => {
+        this.setState(current => ({count: current.count + 1}));
+    };
+    minus = () => {
+        this.setState(current => ({count: current.count - 1}));
+    };
+    render() {
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
     }
-]
-
-function Food({name, picture, rating}) {
-    return (
-        <div>
-            <h1>I Like {name}</h1>
-            <h4>{rating}/5.0</h4>
-            <img src={picture} alt={name}/>
-        </div>
-    )
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number
-}
-
-function App() {
-    return (
-        <div className="App">
-            {foodILike.map(dish => (
-                <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-            ))}
-        </div>
-    );
 }
 
 export default App;
